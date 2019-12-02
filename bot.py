b5c9daa5e2d40d25203aaadf441c9ec177febb5d
Doemuu <43069409+Doemuu@users.noreply.github.com>
2019-12-02 10:37:31 +0100

    Token var

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
 import telegram
+import login
+import configparser
+
+# option 1 : import login file and read TOKEN var
+TOKEN = login.TOKEN
+
+# option 2 : use configparser to read .ini files
+config = configparser.ConfigParser()
+config.read("telegram.ini")
+TOKEN = config["Remindotron"]["TOKEN"]
